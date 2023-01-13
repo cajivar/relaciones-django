9f7671ec16fc61910840f5614d20c7d6326ff524
Christian Jimenez <cajivar@gmail.com>
2023-01-12 20:15:45 -0500

    update inicial

---
+++ movies/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Actor, Director, Genre, Movie
+
+
+admin.site.register(Actor)
+# admin.site.register(Director)
+admin.site.register(Genre)
+# admin.site.register(Movie)
+
+
+class DirectorAdmin(admin.ModelAdmin):
+    search_fields = ['full_name']
+
+admin.site.register(Director, DirectorAdmin)
+
+
+
+class MovieAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'released_year', 'certificate', 'rating', 'genre__name']
+
+admin.site.register(Movie, MovieAdmin)
